9b1ee2337f77429278e07fc7c0dbad483e5b3f05
Levi Leme <levilemes1@gmail.com>
2022-07-12 14:03:47 -0300

    alterando actions secrets e testando record

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    // projectId: "p6xsey",
     e2e: {
         setupNodeEvents(on, config) {
         // implement node event listeners here
