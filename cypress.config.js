72f93a10b8f3388c0b9604fbca1a5abdb1c65d35
Levi Leme <levilemes1@gmail.com>
2022-07-12 12:36:39 -0300

    enviando cypress config

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    projectId: "p6xsey",
+    e2e: {
+        setupNodeEvents(on, config) {
+        // implement node event listeners here
+        },
+        baseUrl: "http://www.vsmshop.com.br",
+    },
+});
